706576db7fe63b21326b94f9f3274cc09f15976f
dannadeily <dannadeilydc@ufps.edu.co>
2021-11-18 08:24:03 -0500

    listar y registrar conferencia

---
+++ vista/listarConferencia.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+
+    <tr>  <th colspan="10"> <h1>listado de conferencia</h1> </th> </tr>
+
+    <tr>
+      <th>ID</th>
+      <th>Nombre</th>
+      <th>Abreviaturas</th>
+      <th>Descripcion</th>
+      <th>Objetivo</th>
+      <th>fecha de inicio</th>
+      <th>fecha fin</th>
+      <th>fecha de pago</th>
+      <th>fecha de enviar articulo</th>
+      <th>fecha respuesta</th>
+      <th>valor inscripcion</th>
+
+    </tr>
+
+    <?php
+
+    $conn=new mysqli("localhost","root","","gestionconferencia");
+
+    if($conn->connect_errno){
+
+      echo "no hay conexion: (". $conn->connect_errno .")".$conn->connect_error;
+    }
+
+      $sql="select * from conferencia";
+      $resultado=mysqli_query($conn,$sql);
+
+      while ($mostrar=mysqli_fetch_array($resultado))
+      {
+     ?>
+     <tr>
+       <td><?php echo $mostrar['id'] ?></td>
+       <td><?php echo $mostrar['nombre'] ?></td>
+       <td><?php echo $mostrar['abreviatura'] ?></td>
+       <td><?php echo $mostrar['descripcion'] ?></td>
+       <td><?php echo $mostrar['objetivo'] ?></td>
+       <td><?php echo $mostrar['fechaInicio'] ?></td>
+       <td><?php echo $mostrar['fechaFin'] ?></td>
+       <td><?php echo $mostrar['fechaPago'] ?></td>
+       <td><?php echo $mostrar['fechaEnvioArticulos'] ?></td>
+       <td><?php echo $mostrar['fechaRespuesta'] ?></td>
+       <td><?php echo $mostrar['valorInscripcion'] ?></td>
+
+     <?php } ?>
+  </body>
+</html>
---
+++ vista/registrarConferencia.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Registrar Producto</title>
+  </head>
+  <body>
+
+    <section id="container">
+
+      <div class="box2">
+      <link rel="stylesheet" href="../vista/css/registrarProducto.css">
+        <h1>Registrar usuario</h1>
+
+      </div>
+
+        <form class="formulario" action="http://localhost/inventario/controlador/?con=productoControlador&fun=guardar" method="POST">
+
+          <label for="nombre">Nombre</label>
+          <input type="text" name="nombre"  id="nombre" placeholder="ingrese el nombre">
+<br>
+          <label for="abreviaturas">Abreviatura</label>
+          <input type="text" name="abreviaturas"  id="abreviaturas"  placeholder="ingrese abreviaturas">
+<br>
+          <label for="descripcion">Descripcion</label>
+          <input type="text" name="descripcion"  id="descripcion"  placeholder="descripcion">
+<br>
+          <label for="objetivo">Objetivo</label>
+          <input type="text" name="objetivo"  id="objetivo" placeholder="objetivo">
+<br>
+          <label for="fechaInicio">fecha de incio</label>
+          <input type="date" name="fechaInicio"  id="fechaInicio"  placeholder="ingrese la fecha de inicio">
+<br>
+          <label for="fechaFin">fecha de fin</label>
+          <input type="date" name="fechaFin"  id="fechaFin"  placeholder="ingrese la fecha de fin">
+          <br>
+          <label for="fechaPago">fecha de pago</label>
+          <input type="date" name="fechaPago"  id="fechaPago"  placeholder="ingrese la fecha de pago">
+          <br>
+          <label for="fechaEnviar">fecha de enviar articulos</label>
+          <input type="date" name="fechaEnviar"  id="fechaEnviar"  placeholder="ingrese la fecha de enviar articulos">
+          <br>
+          <label for="fechaRespuesta">fecha respuesta</label>
+          <input type="date" name="fechaRespuesta"  id="fechaRespuesta"  placeholder="ingrese la fecha de respuesta">
+          <br>
+          <label for="valorInscripcion">Valor inscripcion</label>
+          <input type="text" name="valorInscripcion"  id="valorInscripcion"  placeholder="ingrese el valor de inscripcion">
+<br>
+<br>
+          <input type="submit" name="enviar" value="enviar">
+        </form>
+
+    </section>
+
+  </body>
+</html>
